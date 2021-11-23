aa5d436fb31626df46d75086ffea99313412c579
e.lascano <emilas5678@gmail.com>
2021-11-22 22:30:22 -0500

    successfully render index.html

--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,32 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+
+class Project:
+  def __init__ (self, name, start_date, end_date, description, progress):
+    self.name = name
+    self.start_date = start_date
+    self.end_date = end_date
+    self.description = description
+    self.progress = progress
+    
+
+projects = [
+  Project("Stack 4", "Sept", "Late Sept", "basically a connect 4 game", "Completed"),
+  Project("Music Mania", "Oct", "Nov", "a playlist website", "Not Started"),
+  Project("Partum Projects", "Early Nov", "Late Nov", "A project Organizer", "In Progress"),
+]
+
+
+
+
+
+
 def home(request):
   return HttpResponse('<h1>Heyyyyyy</h1>')
 
 def about(request):
   return render(request, 'about.html')
+
+def projects_index(request):
+  return render(request, 'projects/index.html', { "projects" : projects })
