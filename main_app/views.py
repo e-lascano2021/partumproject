c1591528addc16fccff4a239dc4e1d006b463d9c
e.lascano <emilas5678@gmail.com>
2021-11-23 09:54:17 -0500

    successfully create superuser

--- main_app/views.py
+++ main_app/views.py
@@ -1,25 +1,5 @@
 from django.shortcuts import render
-
-
-
-class Project:
-  def __init__ (self, name, start_date, end_date, description, progress):
-    self.name = name
-    self.start_date = start_date
-    self.end_date = end_date
-    self.description = description
-    self.progress = progress
-    
-
-projects = [
-  Project("Stack 4", "Sept", "Late Sept", "basically a connect 4 game", "Completed"),
-  Project("Music Mania", "Oct", "Nov", "a playlist website", "Not Started"),
-  Project("Partum Projects", "Early Nov", "Late Nov", "A project Organizer", "In Progress"),
-]
-
-
-
-
+from .models import Project
 
 
 def home(request):
@@ -29,4 +9,5 @@ def about(request):
   return render(request, 'about.html')
 
 def projects_index(request):
+  projects = Project.objects.all()
   return render(request, 'projects/index.html', { "projects" : projects })
