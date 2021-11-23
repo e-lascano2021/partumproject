b289508f262a3bf47be34a0d96df4670f8c03dea
e.lascano <emilas5678@gmail.com>
2021-11-23 09:41:54 -0500

    create Progress Model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+PROGRESS = (
+  ('N', 'Not Started'),
+  ('P', 'In Progress'),
+  ('C', 'Completed')
+)
+
+class Project(models.Model):
+  name = models.CharField(max_length=200)
+  start_date = models.DateField('Start Date')
+  end_date = models.DateField('End Date')
+  description = models.TextField(max_length=350)
+  progress = models.CharField(
+  max_length=1,
+  choices=PROGRESS,
+  default=PROGRESS[0][0]
+  )
+  
+  def __str__(self):
+    return self.name
